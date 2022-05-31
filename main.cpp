ad4b1bbb4ca1313f35fb6a170b1eefb8665b26ca
orlowskik <104775955+orlowskik@users.noreply.github.com>
2022-05-31 17:01:31 +0200

    Added documentation

--- main.cpp
+++ main.cpp
@@ -5,131 +5,184 @@
 
 
 
-
+/*
+    Struktura definiujaca pozycje w rankingu filmow
+*/
 struct film{
-    char name[100];
-    double grade;
+    char name[100];  // Tablica znakowa przechowujaca nazwe filmu
+    double grade;    // Ocena danego filmu
 
+    /*
+        Konstruktor struktury
+    */
     film(){
-        grade = 0;
+        grade = 0;  // Przyporzadkowanie 0 pomaga przy pomijaniu blednych wpisow
     }
 
+    /*
+        Konstruktor parametryczny struktury
+        Parametry:
+        [in] char a[] - Tablica znakowa przechowujaca nazwe filmu
+        [in] double b - Ocena filmu
+    */
     film(char a[], double b){
         int i = 0;
-        while(a[i] != EOF){
+        while(a[i] != EOF){         // Petla uzupelniajaca nazwe filmu
             name[i] = a[i];
             ++i;
         }
-        grade = b;
+        grade = b; // Przypisanie oceny
     }
 
 
-    bool operator <(const film& a) const;
+    bool operator <(const film& a) const;   //  Komparator filmow
 };
 
+
+/*
+    Komparator filmow
+
+    Zwraca wartosc TRUE, gdy ocena porownywanego filmu jest wieksza do dwoch liczb po przecinku.
+    W przeciwnym przypadku zwraca FALSE.
+*/
 bool film::operator< (const film& a) const{
     if( this->grade - a.grade  < 0.01)
         return true;
     return false;
 }
 
+/*
 
+
+/*
+    Klasa definiujaca tablice przechowujaca filmy
+
+*/
 class Film_tab{
     private:
-        film *elem; 
-        int amount;
+        film *elem;      // Wskaznik na element struktury typu film
+        int amount;     // Ilosc danych przechowywanych w tablicy
     public:
+        /*
+            Konstruktor parametryczny klasy
+            Parametry:
+            [in] int n - ilosc danych, ktore mozna przechowac w tablicy
+        */
         Film_tab(int n) : elem{new film[n]},amount{n}
         {
-          for(int i=0; i <n; ++i) elem[i] = film();
+          for(int i=0; i <n; ++i) elem[i] = film();  // wywolanie konstruktora struktury film dla kazdego elementu tablicy
         }
-        ~Film_tab(){delete[] elem;}
+        ~Film_tab(){delete[] elem;}     // Destruktor klasy zwalniajacy pamiec calej tablicy
 
-    film& operator[](int i) const;
-    film& operator[](int i);
-    int size() const;
+    film& operator[](int i) const;  //  Operator pozwalajacy na dostep do komuoek tablicy. Tylko do odczytu
+    film& operator[](int i);        //  Operator pozwalajacy na modyfikacje komorek tablicy
+    int size() const {return amount;};               //  Funkcja zwracajaca rozmiar tablicy. Tylko do odczytu
 };  
 
-
+/*
 int Film_tab::size() const{
     return amount;
 }
+*/
 
+/*
+    Operator modyfikujacy wartosci komorek tablicy
+*/
 film& Film_tab::operator[](int i){
-    if(i >= size()){
+    if(i >= size() || i < 0){        // Zabezpieczenie przed przekroczeniem zakresu tablicy
         std::cout << "Tablica poza zakresem!!\n";
         exit(1);
     }
-
-
     return elem[i];
 }
 
+/*
+    Operator dostepu do komorek tablicy tylko do odczytu
+*/
 film& Film_tab::operator[](int i) const{
-    if(i >= size()){
+    if(i >= size() || i < 0){          // Zabezpieczenie przed przekroczeniem zakresu tablicy
         std::cout << "Tablica poza zakresem!!\n";
         exit(1);
     }
-
     return elem[i];
 }
 
 
+/*
+    Funkcja przetwarzajaca dany ciag znakow i dodajaca film do tablicy
+    Parametry:
+    [in] char a[]      - ciag znakow z rankingu filmow
+    [in] Film_tab& tab - referencja do tablicy filmow, gdzie nalezy dodac film
+    [in] int n         - pozycja tablicy, na ktorej ma zostac ustawiony film
 
+    [out] true  - jezeli film zostal dodany prawidlowo
+    [out] false - w przeciwnym wypadku
+
+*/
 bool parse(char a[],Film_tab& tab, int n){
-    film* elem = (film*) malloc(sizeof(film)*n);
-    char number[4];
+    film* elem = (film*) malloc(sizeof(film)*n);  // Alokowanie pamieci na film
+    char number[4];     // Pomocnicza zmienna przechowujaca ocene filmu
     int i = 0, j = 0, k = 0;
 
-    while(a[i] != ',' && a[i] != '\n'){
+    while(a[i] != ',' && a[i] != '\n'){     // Pominiecie numeru wierszu
         ++i;
     }
     i++;
-    if(a[i] == '"'){
+    if(a[i] == '"'){    // W przypadku gdy podany jest serial (nazwa w cudzyslowie)
         ++i;
-        while(a[i] != '"'){
+        while(a[i] != '"'){         // Uzupelnia nazwe filmu az nie napotka konca
             elem->name[j] = a[i];
             ++i;
             ++j;
         }
         ++i;
     }
-    while(a[i] != ',' && a[i] != '\n'){
+    while(a[i] != ',' && a[i] != '\n'){ // Jezeli podana nazwa bez cudzyslowia
         elem->name[j] = a[i];
         ++i;
         ++j;
     }
     ++i;
-    while(a[i] != ',' && a[i] != '\n' && k <= 3){
+    while(a[i] != ',' && a[i] != '\n' && k <= 3){   // Wczytanie oceny filmu, ograniczone do trzech znakow
         number[k] = a[i];
         ++i;
         ++k;
     }
 
-    elem->grade = atof(number);
-    if(elem->grade != 0){
+    elem->grade = atof(number);     // Zamiana oceny na liczbe i przypisanie
+    if(elem->grade != 0){           // Jezeli prawidlowo podao ocene dodaje rekord do tablicy
         tab[n] = *elem;
         return true;
     }
 
 
-    return false;
+    return false;   // W przeciwnym przypadku
 }
 
 
-const int N = 1000000;
-Film_tab pom(N);
+const int N = 10;  // Ilosc wczytywanych filmow
+Film_tab pom(N);   // Tablica pomocniczna uzuwana przy sortowaniu przez scalanie
 
+
+/*
+    Algorytm scalania tablic z filmami wykorzystany w sortowaniu przez scalanie
+    Parametry:
+    [in] Film_tab& base - referencja do tablicy przeznaczonej do sortowania
+    [in] int begin      - pierwsza komorka tablicy
+    [in] int mid        - srodek tablicy
+    [in] int end        - koniec tablicy
+
+*/
 void merge(Film_tab& base,int begin, int mid, int end){
     int i,j;
-    for(int i = begin; i<= end; i++)
+    for(int i = begin; i<= end; i++)    // przepisanie tablicy
         pom[i] = base[i];
-    i = begin; j = mid+1;
+    i = begin; j = mid+1;   // ustawienie wartosci do przeszukiwania tablicy
 
-    for(int k = begin; k <= end; k++){
+    for(int k = begin; k <= end; k++){  // Przechodzi cala podana tablice
         if(i <= mid){
             if(j <= end){
-                base[k] = (pom[j] < pom[i]) ? pom[j++] : pom[i++];
+                base[k] = (pom[j] < pom[i]) ? pom[j++] : pom[i++];  
             }
             else{
                 base[k] = pom[i++];
